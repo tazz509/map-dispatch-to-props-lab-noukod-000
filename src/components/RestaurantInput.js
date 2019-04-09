e3e5d52e94f288de42b520c5ca986332eef02e5a
TAZZ <42774502+tazz509@users.noreply.github.com>
2019-04-09 15:25:17 -0400

    done

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -17,13 +17,13 @@ export class RestaurantInput extends Component {
 
   handleOnLocationChange = event => {
     this.setState({
-      [event.target.id]: event.target.value
+      location: event.target.value
     });
   }
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state);
   }
 
   render() {
@@ -49,6 +49,13 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapStateToProps = state => {
+  return { restaurants: state.restaurants };
+};
+
+ const mapDispatchToProps = dispatch => {
+  return { addRestaurant: (r) => dispatch(addRestaurant(r)) };
+;}
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantInput)
